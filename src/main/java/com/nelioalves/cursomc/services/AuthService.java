47a74ae860632b0be61dc045560cee9faae38b9d
Rodrigo arruda <arruda384digo@gmail.com>
2019-02-21 15:33:11 -0300

    att

--- src/main/java/com/nelioalves/cursomc/services/AuthService.java
+++ src/main/java/com/nelioalves/cursomc/services/AuthService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.repositories.ClienteRepository;
 import com.nelioalves.cursomc.services.exceptions.ObjectNotFoundException;
+import com.nelioalves.cursomc.services.EmailService;
 
 @Service
 public class AuthService {
